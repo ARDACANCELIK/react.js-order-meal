19ea645e89d7b9e978f4dcb35518863d75b9d617
ARDA CAN CELIK <arda6734@hotmail.com>
2023-04-17 14:35:32 +0300

    added cart modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
+import Cart from "./components/Cart/Cart"; //bu ve cart provider aşağısına cart folder koydu o
 
 function App() {
   return (
     <CartProvider>
+      <Cart />
       <Header />
       <main>
         <Meals />
